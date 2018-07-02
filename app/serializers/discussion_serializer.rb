19455e24af717db08480529df1a01965d1daa990
terichadbourne <terichad@gmail.com>
2018-07-02 14:23:13 -0400

    Add `current_user_votes` to discussion serializer
    - Add `current_user_votes` method which returns the number of votes the
    current user has cast for this session
    - Add `current_user_votes` to attributes to be serialized

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class DiscussionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :winner, :editable, :vote_counts, :votes
+  attributes :id, :title, :winner, :editable, :vote_counts, :votes, :current_user_votes
   has_one :discussion_proposer
   has_many :users, through: :votes
+
+  def editable
+    scope == object.discussion_proposer
+  end
 
   def vote_counts
     counts = {}
@@ -17,7 +21,18 @@ class DiscussionSerializer < ActiveModel::Serializer
     counts
   end
 
-  def editable
-    scope == object.discussion_proposer
+  def current_user_votes
+    # if no one is logged in
+    if !scope
+      'no user logged in'
+    # if someone is logged in and has votes on this session, return number of
+    # votes
+    elsif vote_counts[scope.id]
+      vote_counts[scope.id]
+    # if someone is logged in but has no votes for this session, return 0
+    else
+      0
+    end
   end
+
 end
