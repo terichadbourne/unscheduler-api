b4bcfc2f6bc34b0c6767c49c54a3910a16e30bfa
terichadbourne <terichad@gmail.com>
2018-07-02 14:21:47 -0400

    Remove `editable` attribute from vote serializer
    - Remove `editable` method and attribute from vote serializer, as it's
    not being used

--- app/serializers/vote_serializer.rb
+++ app/serializers/vote_serializer.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class VoteSerializer < ActiveModel::Serializer
-  attributes :id, :editable
+  attributes :id
   has_one :user
   has_one :discussion
 
-  def editable
-    scope == object.user
-  end
 end
