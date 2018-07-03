94bf71d5b1481608a9fe82ceb7644418f61dc4ef
terichadbourne <terichad@gmail.com>
2018-07-02 21:26:20 -0400

    Add utility methods to discussion serializer
    - Add utility methods to make it easier to identify needed info in
    Handlebars when generating discussion list
    - Rename `current_user_votes` to `current_user_session_votes`,
    representing the number of votes the current user has cast for this
    particular discussion
    - Create `current_user_total_votes` to represent the total votes that
    user has cast for all sessions
    - Create `current_user_votes_remaining` which subtracts the
    `current_user_total_votes` from a hard-coded value of 5 maximum votes
    - Create `can_upvote` which returns true if
    `current_user_votes_remaining` is positive
    - Create `can_downvote` if `current_user_session_votes` is at least 1
    - TODO: Allow event organizers to set the maximum vote count, replacing
    the hard-coded value just created

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DiscussionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :winner, :editable, :vote_counts, :votes, :current_user_votes
+  attributes :id, :title, :winner, :editable, :vote_counts, :votes, :current_user_session_votes, :current_user_total_votes, :current_user_votes_remaining, :can_upvote, :can_downvote
   has_one :discussion_proposer
   has_many :users, through: :votes
 
@@ -21,17 +21,40 @@ class DiscussionSerializer < ActiveModel::Serializer
     counts
   end
 
-  def current_user_votes
-    # if no one is logged in
-    if !scope
-      'no user logged in'
-    # if someone is logged in and has votes on this session, return number of
-    # votes
-    elsif vote_counts[scope.id]
-      vote_counts[scope.id]
-    # if someone is logged in but has no votes for this session, return 0
-    else
-      0
+  def current_user_session_votes
+    # assuming someone is logged in
+    unless !scope
+      # if they have votes on this session, return number of votes
+      if vote_counts[scope.id]
+        vote_counts[scope.id]
+      # if they have no votes for this session, return 0
+      else
+        0
+      end
+    end
+  end
+
+  def current_user_total_votes
+    if scope
+      scope.votes.count
+    end
+  end
+
+  def current_user_votes_remaining
+    if scope
+      5 - current_user_total_votes
+    end
+  end
+
+  def can_downvote
+    if scope
+      current_user_session_votes >= 1
+    end
+  end
+
+  def can_upvote
+    if scope
+      current_user_votes_remaining.positive?
     end
   end
 
